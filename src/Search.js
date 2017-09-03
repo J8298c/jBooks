d7a78c35a3a55d234c8b5a42a76e8c570c02c9a5
j8298c <juliocmojica@gmail.com>
2017-09-03 16:21:46 -0400

    search component search component displays results

--- src/Search.js
+++ src/Search.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
+import * as BooksAPI from './BooksAPI';
+import Book from './Book';
 
 class Search extends Component {
   state = {
     query: '',
+    searchResults: [],
   };
+  updateQuery(query) {
+    console.log(query);
+    BooksAPI.search(query).then((books) => { this.setState({ searchResults: books})})
+  }
   render() {
+    const { searchResults } = this.state;
+    const displayBooks = searchResults.map((book) => (
+      <Book bookImage={book.imageLinks.thumbnail} bookAuthor={book.authors} bookTitle={book.title} key={book.id} />
+    ))
+    console.log(this.state.searchResults, 'the search results');
     return (
       <div className="search-books">
       <div className="search-books-bar">
         <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
         <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title or author" onChange={(event) => { this.setState({query: event.target.value})}}/>
+          <input type="text" placeholder="Search by title or author" 
+          onChange={(event) => {this.updateQuery(event.target.value)}}
+          />
         </div>
       </div>
       <div className="search-books-results">
         <ol className="books-grid">
-
+          { searchResults ? displayBooks : '..Search for a Book'  }
         </ol>
       </div>
     </div>
