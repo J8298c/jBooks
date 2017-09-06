b213161b4f6b9a187d594768c804d1f13e3afe98
j8298c <juliocmojica@gmail.com>
2017-09-06 09:25:08 -0400

    added booksapi and method to search books in bookSearchComponent

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import * as BooksAPI from './BooksAPI';
 
 class BookSearch extends Component {
   state = {
     query: '',
   }
+  onSearch(searchQuery) {
+    BooksAPI.search(searchQuery);
+  }
   render() {
     return (
-      <div>
-        BookSearch
-      </div>
+      <div className="search-books">
+            <div className="search-books-bar">
+              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              <div className="search-books-input-wrapper">
+                <input type="text" placeholder="Search by title or author"
+                onChange={(event) => { this.onSearch(event.target.value)}}/>
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
+          </div>
     )
   }
 }
--- src/BookSearch.test.js
+++ src/BookSearch.test.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import {shallow } from 'enzyme';
 import { expect } from 'chai';
 
 import BookSearch from './BookSearch';
 
 describe('BookSearch should render', () => {
-  const wrapper = mount(<BookSearch />)
+  const wrapper =shallow(<BookSearch />)
   it('should render without exploding', () => {
-    expect(wrapper.find('div')).to.have.length(1);
+    expect(wrapper.find('div')).to.have.length(4);
+    expect(wrapper.find('ol')).to.have.length(1);
   })
 })
