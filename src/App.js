5c0f03b74b53d0c323ab615c00fd4515077177f4
j8298c <juliocmojica@gmail.com>
2017-09-06 21:31:03 -0400

    back to the drawinging board

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-import * as BooksAPI from './BooksAPI'
+// import * as BooksAPI from './BooksAPI'
 import './App.css'
-import Book from './Book';
 
 class BooksApp extends React.Component {
   state = {
-      books: [],
-  };
-    componentWillMount() {
-      BooksAPI.getAll()
-          .then((books) => { this.setState({ books})})
-    }
-    onShelfChange(book,shelf) {
-        console.log(book, shelf, 'parameters being passed into function');
-        BooksAPI.update(book, shelf);
-        BooksAPI.getAll().then((books) => { console.log(books)});
-    }
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    showSearchPage: false
+  }
+
   render() {
-        const {books} = this.state;
-        console.log(books, 'the books from the state');
     return (
+      <div className="app">
+        {this.state.showSearchPage ? (
+          <div className="search-books">
+            <div className="search-books-bar">
+              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              <div className="search-books-input-wrapper">
+                {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                <input type="text" placeholder="Search by title or author"/>
+
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
+          </div>
+        ) : (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -31,19 +48,12 @@ class BooksApp extends React.Component {
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                        {/*
-                        Book
-                        */}
-                        <Book bookTitle='To Kill A Mockingbird' bookAuthor='Julio'
-                          onChange={(event) => { this.onShelfChange(this, event.target.value)}}
-                          imageUrl='http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'
-                          />
                       <li>
                         <div className="book">
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
                             <div className="book-shelf-changer">
-                              <select onChange={(event) => { this.onShelfChange(this.bookTitle, event.target.value)}}>
+                              <select>
                                 <option value="none" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
@@ -56,9 +66,6 @@ class BooksApp extends React.Component {
                           <div className="book-authors">Harper Lee</div>
                         </div>
                       </li>
-                        {/*
-                        end of book div
-                        */}
                       <li>
                         <div className="book">
                           <div className="book-top">
@@ -187,9 +194,11 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <Link to='/search'>Add a book</Link>
+              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
             </div>
           </div>
+        )}
+      </div>
     )
   }
 }
