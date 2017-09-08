64db8371f93cf0e639d027cecdb70c305c010f2c
j8298c <juliocmojica@gmail.com>
2017-09-08 14:07:20 -0400

    rendering books and shelves
    using components to render books and shelves

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Book from './Book';
 
 function Shelf(props) {
   console.log(props, 'props in the shelf component');
+  const showBooks = props.books.map((book) => (
+    <Book bookTitle={book.title} bookAuthor={book.authors}
+      imageUrl={book.imageLinks.thumbnail} key={book.id}
+    />
+  ))
   return(
     <div className="bookshelf">
       <h2 className="bookshelf-title">{props.shelfName}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
+          {showBooks}
         </ol>
       </div>
     </div>
