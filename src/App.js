d24b2abcc3727051c21cc94f49215cf3c4a27fbd
j8298c <juliocmojica@gmail.com>
2017-09-24 12:49:41 -0400

    removing bookdemo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import SearchBook from './SearchBook';
 import Shelves from './Shelves';
-import BookDemo from './BookDemo';
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -12,29 +11,34 @@ class BooksApp extends React.Component {
     this.state = {
       books: [],
       searchResults: [],
+      book: [],
     };
     this.onShelfChange = this.onShelfChange.bind(this);
     this.onSearch = this.onSearch.bind(this);
+    this.getAllBookOnShelf = this.getAllBookOnShelf.bind(this);
   }
 
-      componentDidMount() {
-          BooksAPI.getAll()
-              .then((books) => { this.setState({ books })});
-      }
-
-      onShelfChange(id, shelf) {
-        BooksAPI.update({id: id}, shelf).then((books) => {
-            BooksAPI.getAll()
-              .then((books) => { this.setState({ books })})
-          })
-    }
-  
+  componentDidMount() {
+    this.getAllBookOnShelf();
+  }
+  onShelfChange(id, shelf) {
+    console.log(id, shelf)
+    BooksAPI.update({id}, shelf)
+    this.getAllBookOnShelf();
+  }
+  getAllBookOnShelf() {
+    BooksAPI.getAll()
+      .then((books) => { this.setState({books})});
+  }
   onSearch(query) {
     if (query !== ''){
       BooksAPI.search(query, 10).then((results) => {this.setState({searchResults: results})})
     }
   }
-
+  getABook(bookID) {
+    BooksAPI.get(bookID)
+      .then((book) => { this.setState({book})})
+  }
   render() {
     const { books } = this.state;
     return (
@@ -49,7 +53,6 @@ class BooksApp extends React.Component {
           <Route exact path='/' render={() => (
             <Shelves books={books} onShelfChange={this.onShelfChange}/>
           )}/>
-          <Route path='/book/:id' component={BookDemo} />
       </div>
     )
   }
