518f68ef4b928ccc77b0d63e38f86d64b2371c9a
j8298c <juliocmojica@gmail.com>
2017-09-26 10:40:31 -0400

    app.js methods
    commments as to what each method handles for future refrence

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class BooksApp extends React.Component {
       books: [],
       searchResults: [],
       book: [],
+      read: [],
     };
     this.onShelfChange = this.onShelfChange.bind(this);
     this.onSearch = this.onSearch.bind(this);
@@ -22,17 +23,43 @@ class BooksApp extends React.Component {
   componentDidMount() {
     this.getAllBookOnShelf();
   }
-  onShelfChange(book, shelf) {
-    BooksAPI.update({id: book}, shelf).then((books) => {
+  /**
+   * handles switching the current shelf for books
+   * 
+   * 
+   * @param {any} id
+   * @param {any} shelf 
+   * @memberof BooksApp
+   */
+  onShelfChange(id, shelf) {
+    BooksAPI.update({id: id}, shelf).then((books) => {
       BooksAPI.getAll()
-      .then((books) => { this.setState({ books })})
+      .then((books) => { 
+        this.setState({ books })
+        this.checkInBooks(books);
+      })
     })
   }
+
+  /**
+   * returns all books from current api endpoint
+   * 
+   * @memberof BooksApp
+   */
 
   getAllBookOnShelf() {
     BooksAPI.getAll()
       .then((books) => { this.setState({books})});
   }
+
+  /**
+   * takes in user input when search for a book 
+   * and displays the results has a helper function
+   * called check in books
+   * 
+   * @param {any} query 
+   * @memberof BooksApp
+   */
   onSearch(query) {
     if (query !== ''){
       BooksAPI.search(query, 10)
@@ -44,23 +71,47 @@ class BooksApp extends React.Component {
       })
     }
   }
+
+  /**
+   * takes in array of books from search results
+   * maps through and adds key shelf and value none
+   * to books which dont have a shelf value
+   * 
+   * @param {any} books 
+   * @memberof BooksApp
+   */
   checkInBooks(books) {
     const results = [];
+    const read = [];
     books.map((book) => {
       if(!book.shelf){
         book.shelf = 'none';
         results.push(book);
-      }
-      return results;
+        return results;
+      } else if (book.shelf = read){
+        read.push(book);
+        this.setState({read})
+      }    
     })
   }
+
+  /**
+   * finds a single book to display
+   * within the bookdemo page
+   * 
+   * @param {any} id 
+   * @memberof BooksApp
+   */
   getABook(id) {
     console.log(id, 'param id')
     BooksAPI.get(id)
       .then((book) => { console.log(book), this.setState({book})})
   }
+
+  
   render() {
     console.log(this.state.book, 'single book');
+    console.log(this.state.read, 'read books');
     const { books } = this.state;
     return (
       <div className="app">
