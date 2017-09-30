478b286c28b2712003f06fdfa566f87d6ee8f386
j8298c <juliocmojica@gmail.com>
2017-09-30 11:35:57 -0400

    cleaning up and refactoring

--- src/App.js
+++ src/App.js
@@ -1,70 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI';
 import Shelves from './Shelves';
-import SearchBook from './SearchBook';
-import './App.css'
+import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: {
-        read: [],
-        wantToRead: [],
-        currentlyReading: [],
-      },
-      searchResults: [],
+      books: [],
     }
     this.stockShelves = this.stockShelves.bind(this);
-    this.onSearch = this.onSearch.bind(this);
-    this.addShelftoBooks = this.addShelftoBooks.bind(this);
     this.onShelfChange = this.onShelfChange.bind(this);
   }
-
   componentDidMount() {
-    this.stockShelves()
+    this.stockShelves();
   }
 
   stockShelves() {
-    BooksAPI.getAll()
-      .then(books => {
-        const read = this.librarian(books, 'read');
-        const wantToRead = this.librarian(books, 'wantToRead');
-        const currentlyReading = this.librarian(books, 'currentlyReading');
-        this.setState({
-          books: {
-            read,
-            currentlyReading,
-            wantToRead
-          }
-        })
-      })
-  }
-
-  librarian(books, shelfName) {
-    return books.filter(book => book.shelf === shelfName)
-  }
-
-  onSearch(query) {
-    if(query !== ''){
-      BooksAPI.search(query)
-      .then(results => {
-        this.addShelftoBooks(results);
-        this.setState({searchResults: results});
-      })
-    }
-  }
-
-  addShelftoBooks(books){
-    const results =[];
-    books.map(book => {
-      if(!book.shelf){
-        book.shelf = 'none';
-        results.push(book);
-      } 
-      return results;
-    })
+    BooksAPI.getAll().then(books => this.setState({ books }));
   }
 
   onShelfChange(id, shelf) {
@@ -73,16 +27,11 @@ class App extends Component {
       this.stockShelves();
     })
   }
+  
 
   render() {
     return (
       <div className="app">
-          <Route path='/search' render={() => (
-            <SearchBook 
-              onChange={(event) => {this.onSearch(event.target.value)}} onShelfChange={this.onShelfChange} searchResults={this.state.searchResults}
-              books={this.state.books}
-            />
-          )}/>
           <Route exact path='/' render={() => (
             <Shelves books={this.state.books} onShelfChange={this.onShelfChange}/>
           )}/>
@@ -91,4 +40,3 @@ class App extends Component {
   }
 }
 export default App;
-
